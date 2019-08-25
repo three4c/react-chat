41a5a656a6e6068751f76fc5fc441df7b477c481
three4c <three4c@gmail.com>
2019-08-26 03:02:51 +0900

    .env化

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Firebase Hosting :  [https://react-chat-110a6.firebaseapp.com/](https://react-chat-110a6.firebaseapp.com/)
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDCWMOST7iBiglhTNRpquesLqGjITxkZ00",
-    authDomain: "react-chat-110a6.firebaseapp.com",
-    databaseURL: "https://react-chat-110a6.firebaseio.com",
-    projectId: "react-chat-110a6",
-    storageBucket: "",
-    messagingSenderId: "440612242797",
-    appId: "1:440612242797:web:4f3078dc26812e5d"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
